f0b422e2e2819d0e6665a39f2607bc295a358e67
HasturTheOld <leasthappy1@gmail.com>
2026-06-23 04:50:08 -0700

    Update cloud.py

--- cloud.py
+++ cloud.py
@@ -1,4 +1,3 @@
-# cloud.py
 import boto3
 import os
 from dotenv import load_dotenv
@@ -17,7 +16,6 @@ class YandexCloudStorage:
         )
     
     def upload_file(self, file_path, object_name):
-        """Загрузить файл в Yandex Cloud"""
         try:
             self.client.upload_file(file_path, self.bucket_name, object_name)
             url = f"https://storage.yandexcloud.net/{self.bucket_name}/{object_name}"
@@ -26,24 +24,8 @@ class YandexCloudStorage:
             return {'success': False, 'error': str(e)}
     
     def delete_file(self, object_name):
-        """Удалить файл из Yandex Cloud"""
         try:
             self.client.delete_object(Bucket=self.bucket_name, Key=object_name)
             return {'success': True}
         except Exception as e:
             return {'success': False, 'error': str(e)}
-    
-    def get_metadata(self, object_name):
-        """Получить информацию о файле"""
-        try:
-            response = self.client.head_object(
-                Bucket=self.bucket_name,
-                Key=object_name
-            )
-            return {
-                'size': response.get('ContentLength'),
-                'last_modified': response.get('LastModified'),
-                'content_type': response.get('ContentType')
-            }
-        except Exception as e:
-            return {'error': str(e)}
